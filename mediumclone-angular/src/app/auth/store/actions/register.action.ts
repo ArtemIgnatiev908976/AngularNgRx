b922f4d74498c19b1fd3d90724d5e525fc15966e
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-09 20:33:39 +0400

    Создание интерфейса для модуля авторизации

--- mediumclone-angular/src/app/auth/store/actions/register.action.ts
+++ mediumclone-angular/src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import {createAction, props} from "@ngrx/store";
 import {ActionsTypes} from "../actionTypes";
+import {RegisterRequestInterface} from "../../types/registerRequest.interface";
 
 export const registerAction = createAction(
   ActionsTypes.REGISTER,
-  props<{ username: string; email: string, password: string }>()
+  props<RegisterRequestInterface>()
 )

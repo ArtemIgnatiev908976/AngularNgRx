113c1f43f019f330d4c1e37edf4144c4c5929f81
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-09 19:29:11 +0400

    Как дебажить ngRx

--- mediumclone-angular/src/app/app.module.ts
+++ mediumclone-angular/src/app/app.module.ts
@@ -4,11 +4,14 @@ import {NgModule} from '@angular/core'
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {AuthModule} from 'src/app/auth/auth.module';
-import { StoreModule } from '@ngrx/store'
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })

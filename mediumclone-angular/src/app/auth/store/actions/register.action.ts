ba53ec9933e1ae5d9bbe210f8eb1178a8e47f7a0
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-09 18:47:39 +0400

    Установка ngRx

---
+++ mediumclone-angular/src/app/auth/store/actions/register.action.ts
@@ -0,0 +1,7 @@
+import {createAction, props} from "@ngrx/store";
+import {ActionsTypes} from "../actionTypes";
+
+export const registerAction = createAction(
+  ActionsTypes.REGISTER,
+  props<{ username: string; email: string, password: string }>()
+)

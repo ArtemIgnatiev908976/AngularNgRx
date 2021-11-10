475bac2d16f3b0d1db9a9841158fd793cb70aa29
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-10 17:26:31 +0400

    Создание селекторов для модуля авторизации

--- mediumclone-angular/src/app/auth/components/register/register.component.ts
+++ mediumclone-angular/src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core'
 import {FormGroup, FormBuilder, Validators} from '@angular/forms'
-import {Store} from "@ngrx/store";
+import {select, Store} from "@ngrx/store";
 import {registerAction} from "../../store/actions/register.action";
+import {Observable} from "rxjs";
+import {isSubmittingSelector} from "../../store/selectors";
 
 @Component({
   selector: 'mc-register',
@@ -11,10 +13,17 @@ import {registerAction} from "../../store/actions/register.action";
 export class RegisterComponent implements OnInit {
   form: FormGroup
 
+  isSubmitting$: Observable<boolean> // наблюдатель с типом boolean
+
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
+    this.initializeValues()
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector)) //выбираем данные по селектору из стора и сеттит в наблюдателя
   }
 
   initializeForm(): void {

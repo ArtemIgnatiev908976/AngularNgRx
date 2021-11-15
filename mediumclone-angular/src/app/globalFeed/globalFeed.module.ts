50a8e99ded760ecd7f2255c763a07580ca5b9cb2
IgnatevArtem <ignatev.artem.nik@yandex.ru>
2021-11-16 01:45:42 +0400

    Создание модуля фида

---
+++ mediumclone-angular/src/app/globalFeed/globalFeed.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import {CommonModule} from "@angular/common";
+import { GlobalFeedComponent } from './components/global-feed/globalFeed.component';
+import {RouterModule} from "@angular/router";
+import {FeedModule} from "../shared/modules/feed/feed.module";
+const routes = [
+  {
+    path: '',
+    component: GlobalFeedComponent
+  }
+]
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes), FeedModule],
+  declarations: [GlobalFeedComponent]
+})
+export class GlobalFeedModule{}

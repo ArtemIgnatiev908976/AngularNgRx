05a7887ae0f982bcc31ae5f2fd6bbfbef95d3fcd
IgnatievArtem908976 <ignatev.artem.nik@gmail.com>
2021-11-16 23:12:14 +0400

    Подготовка стора для фида

--- mediumclone-angular/src/app/shared/modules/feed/components/feed/feed.component.ts
+++ mediumclone-angular/src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FeedComponent {}
--- mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
+++ mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
@@ -1,12 +1,21 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {FeedComponent} from './components/feed/feed.component';
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {EffectsModule} from '@ngrx/effects'
 
+import {FeedComponent} from 'src/app/shared/modules/feed/components/feed/feed.component'
+import {FeedService} from 'src/app/shared/modules/feed/services/feed.service'
+import {GetFeedEffect} from 'src/app/shared/modules/feed/store/effects/getFeed.effect'
+import {StoreModule} from '@ngrx/store'
+import {reducers} from 'src/app/shared/modules/feed/store/reducers'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers)
+  ],
   declarations: [FeedComponent],
-  exports: [FeedComponent]
+  exports: [FeedComponent],
+  providers: [FeedService]
 })
-export class FeedModule {
-}
+export class FeedModule {}

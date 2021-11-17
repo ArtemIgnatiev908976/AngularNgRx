2a3914376f2dacfc2a94a0dfa6a4ea9485cb081d
IgnatievArtem908976 <ignatev.artem.nik@gmail.com>
2021-11-17 15:45:57 +0400

    Получаем данные фида для отрисовки

--- mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
+++ mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
@@ -1,18 +1,20 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {EffectsModule} from '@ngrx/effects'
+import {RouterModule} from '@angular/router'
+import {StoreModule} from '@ngrx/store'
 
 import {FeedComponent} from 'src/app/shared/modules/feed/components/feed/feed.component'
 import {FeedService} from 'src/app/shared/modules/feed/services/feed.service'
 import {GetFeedEffect} from 'src/app/shared/modules/feed/store/effects/getFeed.effect'
-import {StoreModule} from '@ngrx/store'
 import {reducers} from 'src/app/shared/modules/feed/store/reducers'
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forFeature([GetFeedEffect]),
-    StoreModule.forFeature('feed', reducers)
+    StoreModule.forFeature('feed', reducers),
+    RouterModule
   ],
   declarations: [FeedComponent],
   exports: [FeedComponent],

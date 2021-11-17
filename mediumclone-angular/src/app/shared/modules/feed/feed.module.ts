243975fc1aa557f171366a445266c148e2e7a127
IgnatievArtem908976 <ignatev.artem.nik@gmail.com>
2021-11-17 16:33:20 +0400

    Добавление вспомогательных компонентов

--- mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
+++ mediumclone-angular/src/app/shared/modules/feed/feed.module.ts
@@ -8,13 +8,17 @@ import {FeedComponent} from 'src/app/shared/modules/feed/components/feed/feed.co
 import {FeedService} from 'src/app/shared/modules/feed/services/feed.service'
 import {GetFeedEffect} from 'src/app/shared/modules/feed/store/effects/getFeed.effect'
 import {reducers} from 'src/app/shared/modules/feed/store/reducers'
+import {ErrorMessageModule} from 'src/app/shared/modules/errorMessage/errorMessage.module'
+import {LoadingModule} from 'src/app/shared/modules/loading/loading.module'
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
-    RouterModule
+    RouterModule,
+    ErrorMessageModule,
+    LoadingModule
   ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
